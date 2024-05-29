238110288bfb4ca6ae36a2b356856a0f4a7aa866
sirNewbies <yusromi04@gmail.com>
2024-05-29 11:39:50 +0700

    Use gform for reporting bug[2]

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -2,6 +2,6 @@ export const SITE_CONFIG = {
     REPOSITORY_URL: "https://github.com/OrionOS-prjkt/bug_report",
     CREATE_ISSUE: (title: string) =>
         `https://github.com/OrionOS-prjkt/bug_report/issues/new?title=${title}&labels=feedback`,
-    FORM: "https://github.com/OrionOS-prjkt/bug_report/form",
+    FORM: "https://forms.gle/GWi7Dt8b3K6rToy6A",
     DELETE_FORM: "https://github.com/OrionOS-prjkt/bug_report/delete",
 } as const;
